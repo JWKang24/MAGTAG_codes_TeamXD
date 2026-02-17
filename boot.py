50b183dd42b540ae4950ebe9cacd2cf4294243bd
ejones02 <ejones02@stanford.edu>
2026-02-16 16:50:40 -0800

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,13 +1,16 @@
+storage.remount("/", not button.value)
 import storage
+import supervisor
 import board
 import digitalio
 
-# Use Button A (D15) as our "Computer Override" switch
+# Setup a physical override button (Button A)
 button = digitalio.DigitalInOut(board.D15)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 
-# Logic:
-# If Button A is pressed (value is False), the computer can write (readonly=True for MCU).
-# If Button A is NOT pressed (value is True), the MagTag can write (readonly=False for MCU).
-storage.remount("/", not button.value)
+# If Button A is held OR a laptop is detected, laptop gets write access.
+if not button.value or supervisor.runtime.serial_connected:
+    storage.remount("/", True)
+else:
+    storage.remount("/", False)
